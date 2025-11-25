a79202f65393d496a5befe5d4a542e2c373fb24f
Veronika <panicka144@gmail.com>
2025-11-25 22:05:03 +0300

    readme upd + cleaning

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Ждем пока MySQL будет готова
 echo "Waiting for MySQL to be ready..."
 while ! nc -z mysql 3306; do
   sleep 1
 done
 echo "MySQL is ready!"
 
-# Применяем миграции
+# миграции
 echo "Applying database migrations..."
 python manage.py migrate
 
-# Запускаем сервер
+# сервер
 echo "Starting server..."
 python manage.py runserver 0.0.0.0:8000
